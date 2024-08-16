b71f50946b6ba5a4b4215c85adcde01efde94e5a
Ivaylo Georgiev <ivailo1411@gmail.com>
2024-08-16 17:47:10 +0300

    Small changes

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
 import Btn from './components/Button/Button'
 import { AppContext } from './context/authContext'
+import DrawerNav from './components/Drawer/Drawer'
 
 function App() {
 
   
   return (
     <AppContext.Provider>
-            <h1>Website</h1>
+      <h1>Final Project</h1>
+      <h2>Team 2</h2>
+      <DrawerNav />
+      <Routes>
+        {/* <Route path='/' element={} /> */}
+
+        {/* <Route path='/userboard' element={} /> */}
+        {/* <Route path='/register' element={} /> */}
+      </Routes>
             <Btn />
     </AppContext.Provider>
   )
