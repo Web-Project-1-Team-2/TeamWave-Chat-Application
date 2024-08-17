99b2df9be9374444e018b39cd7dcca31ec17e0aa
Ivo Dimitrov <lautaspartan@gmail.com>
2024-08-17 16:37:25 +0300

    Landing page

--- final-project/src/App.jsx
+++ final-project/src/App.jsx
@@ -5,23 +5,25 @@ import Btn from './components/Button/Button'
 import { AppContext } from './context/authContext'
 import DrawerNav from './components/Drawer/Drawer'
 import Register from './pages/Register/Register'
+import SignIn from './pages/Register/LandingPage/LandingPage'
 
 function App() {
 
-  
+
   return (
     <AppContext.Provider>
       <h1>Final Project</h1>
       <h2>Team 2</h2>
       <DrawerNav />
       <Routes>
+        <Route path="/" element={<SignIn />} />
         {/* <Route path='/' element={} /> */}
 
         {/* <Route path='/userboard' element={} /> */}
         {/* <Route path='/register' element={} /> */}
       </Routes>
-            {/* <Btn /> */}
-            <Register />
+      {/* <Btn /> */}
+      <Register />
     </AppContext.Provider>
   )
 }
