1b53183f430e04aced7611dce2469f5078f4e62a
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-20 17:08:51 +0300

    Created and implemented UploadAvatar functionality and storage service

--- final-project/src/pages/Profile/Profile.jsx
+++ final-project/src/pages/Profile/Profile.jsx
@@ -1,34 +1,33 @@
-import { useContext, useEffect, useState } from 'react';
-import { AppContext } from '../../context/authContext';
-import { Typography } from '@mui/material';
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../context/authContext";
+import { Typography } from "@mui/material";
+
+
 
 const Profile = () => {
 
-    const { userData } = useContext(AppContext);
-    const [data, setData] = useState({
-        avatar: '',
-        uid: '',
-        username: '',
+
+  const { userData } = useContext(AppContext);
+  const [data, setData] = useState({
+    avatar: "",
+    uid: "",
+    username: "",
+  });
+
+  useEffect(() => {
+    if (!userData) return;
+    setData({
+      ...userData,
+      avatar: userData.avatar || "",
+      username: userData.username || "",
+      uid: userData.uid || "",
     });
+  }, [userData]);
 
-    useEffect(() => {
-        if (!userData) return;
-        setData({
-            ...userData,
-            avatar: userData.avatar || '',
-            username: userData.username || '',
-            uid: userData.uid || '',
-        });
-    }, [userData]) 
-    
-    return (
-        <div>
-            <Typography variant='h1'>Profile</Typography>
-            <Typography variant='h5'>Username: {data.username}</Typography>
-            <Typography variant='h5'>First Name: {data.firstName}</Typography>
-            <Typography variant='h5'>Last Name: {data.lastName}</Typography>
-        </div>
-    )
-}
+  return (
+    <Typography>{data.username}</Typography>
+  )
+};
 
-export default Profile
+export default Profile;
+
