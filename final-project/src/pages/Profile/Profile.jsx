52689cf6b04e04cb675077c01f2be2afe7cb0f3a
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-20 17:10:54 +0300

    Implemented UploadAvatar functionality in Profile page

--- final-project/src/pages/Profile/Profile.jsx
+++ final-project/src/pages/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../context/authContext";
-import { Typography } from "@mui/material";
-
+import { Avatar, Box, Typography } from "@mui/material";
+import UploadAvatar from "../../components/UploadAvatar/UploadAvatar";
 
 
 const Profile = () => {
-
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const { userData } = useContext(AppContext);
   const [data, setData] = useState({
@@ -25,8 +27,27 @@ const Profile = () => {
   }, [userData]);
 
   return (
-    <Typography>{data.username}</Typography>
-  )
+    <Box width="100%" display="flex" flexDirection="column" alignItems="center">
+      <Typography variant="h2" mb={3}>
+        Profile
+      </Typography>
+
+      <Box display="flex" alignItems="center" mb={2}>
+        <Avatar
+          src={data.avatar}
+          sx={{ width: 100, height: 100, mr: 2, cursor: "pointer" }}
+          onClick={handleOpen}
+        >
+          {!data.avatar &&
+            (data.firstName
+              ? data.firstName[0].toUpperCase() + data.lastName[0].toUpperCase()
+              : "A")}
+          
+        </Avatar>
+      </Box>
+      <UploadAvatar open={open} handleClose={handleClose} avatar={data.avatar} username={data.username} uid ={data.uid}/>
+    </Box>
+  );
 };
 
 export default Profile;
