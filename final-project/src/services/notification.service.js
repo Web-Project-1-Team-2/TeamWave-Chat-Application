c898952b3b4a1bcb088b6bc3835c941dac954f9a
plamenggg <plamen_ganchev_94@abv.bg>
2024-08-24 12:49:59 +0300

    Added notification service

---
+++ final-project/src/services/notification.service.js
@@ -0,0 +1,25 @@
+import { toast, Bounce } from 'react-toastify';
+
+export const notifyError = (message) => toast.error(message, {
+    position: "bottom-right",
+    autoClose: 4000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+});
+
+export const notifySuccess = (message) => toast.success(message, {
+    position: "bottom-right",
+    autoClose: 1000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    transition: Bounce,
+    });
